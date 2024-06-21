44373b904dae0eb8edbff29685221a26feb1a600
JulieElmgaard <91532152+JulieElmgaardJensen@users.noreply.github.com>
2024-06-21 13:28:25 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 // Ændre til http://localhost:5173 hvis lokalt
 app.use(cors({
-  origin: process.env.APP_URL,
+  origin: "https://full-stack-app-ef9y.onrender.com",
   credentials: true,
 }));
 
