4ff18c49d2ae86de210e7c55c8d0b9711c46c584
JulieElmgaardJensen <julieelmgaardjensen@gmail.com>
2024-06-21 12:17:43 +0200

    env

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 // Ændre til http://localhost:5173 hvis lokalt
 app.use(cors({
-  origin: `process.env.APP_URL`, 
+  origin: process.env.APP_URL,
   credentials: true,
 }));
 
