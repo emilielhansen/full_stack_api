85ae4d9a7c5ab8d6a485077a9cda2c69ccd06e3e
JulieElmgaard <91532152+JulieElmgaardJensen@users.noreply.github.com>
2024-06-22 12:22:55 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 // Ændre til http://localhost:5173 hvis lokalt
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.APP_URL,
   credentials: true,
 }));
 
