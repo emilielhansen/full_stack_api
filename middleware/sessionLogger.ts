b93485011468b50f460d793d4da5275557b44c09
JulieElmgaardJensen <julieelmgaardjensen@gmail.com>
2024-06-23 21:44:56 +0200

    uuid

--- middleware/sessionLogger.ts
+++ middleware/sessionLogger.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-//Logger i terminalen
+//Logger i terminalen i vscode
 const sessionLogger = (req: Request, res: Response, next: NextFunction) => {
   console.log('Session data:', req.session);
   next();
